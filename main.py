b06f412a5a98ee9ecf7cf7fa614dcaecb530bda1
Gerard <dothanhvinh1@gmail.com>
2023-04-23 17:22:11 +0700

    Send aritcle titles and descriptions via email

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import requests
+from sen_email import send_email
+
 
 API_KEY = "60c37095be17431f96ed910a7f35c805"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-20&sortBy=publishedAt&apiKey=60c37095be17431f96ed910a7f35c805"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-28&sortBy=publishedAt&apiKey=60c37095be17431f96ed910a7f35c805"
 
 request = requests.get(url)
 content = request.json()
+titles = []
+descriptions =[]
+text = ""
+print(content)
 for article in content["articles"]:
-    print(article["author"])
+    text = text + article["title"] + "\n" + article["description"] + 2*"\n"
+
+message = f"""
+Subject: Articles about Tesla
+
+{text}
+"""
+message = message.encode("utf-8")
+send_email(message)
---
+++ sen_email.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+password_env = os.getenv("PASSWORD")
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "dothanhvinh1@gmail.com"
+    password = password_env
+    receiver = "dothanhvinh1@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
